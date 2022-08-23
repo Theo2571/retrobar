8940c7cfe4190d0086505e63fcb0c7c812aed9db
Roman Gorbachev <rn.gorbachev@gmail.com>
2022-08-23 15:24:00 +0600

    RES-6-PhotoPage-roman: Created ui component lowerBlock with icons

---
+++ src/pages/PhotoPage/PhotoPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./PhotoPage.module.scss"
+import LowerBlock from "../../components/ui/lower-block/lowerBlock";
+
+
+const PhotoPage = () => {
+    return (
+        <div className={styles.container}>
+            <LowerBlock/>
+        </div>
+    );
+};
+
+export default PhotoPage;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -9,23 +9,31 @@ import {
   ADMIN_POSTER_PAGE,
   ADMIN_VIDEO_PAGE,
   MAIN_PAGE,
+  PHOTO_PAGE,
 } from "../consts/consts";
 
 import AdminAuthPage from "../pages/AdminAuthPage/AdminAuthPage"
 import AdminPhotoPage from "../pages/AdminPhotoPage/AdminPhotoPage";
+import PhotoPage from "../pages/PhotoPage/PhotoPage";
+import {useId} from "react";
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
     {
       path: ADMIN_AUTH_PAGE,
       Component: <AdminAuthPage />,
-      id: 1,
+      id: useId(),
     },
     {
       path: ADMIN_PHOTO_PAGE,
       Component: <AdminPhotoPage />,
-      id: 2,
+      id: useId(),
     },
+    {
+      path: PHOTO_PAGE,
+      Component: <PhotoPage />,
+      id: useId(),
+    }
   ];
 
   return (
