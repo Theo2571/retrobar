4777f81336b0d8a0ee5381acc3d6de3828223215
Roman Gorbachev <rn.gorbachev@gmail.com>
2022-08-23 15:35:05 +0600

    RES-6-PhotoPage-roman: Created ui component titleForPages.tsx

--- src/pages/PhotoPage/PhotoPage.tsx
+++ src/pages/PhotoPage/PhotoPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from "./PhotoPage.module.scss"
 import LowerBlock from "../../components/ui/lower-block/lowerBlock";
+import TitleForPages from "../../components/ui/title-for-pages/titleForPages";
 
 
 const PhotoPage = () => {
     return (
         <div className={styles.container}>
+            <TitleForPages title={'Актуальные фотоотчеты'}/>
             <LowerBlock/>
         </div>
     );
